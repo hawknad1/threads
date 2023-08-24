f5cc0f977378ffedaa0e6294e29b90929fbdaaf3
Thomas Dankwah <dankwahthom@gmail.com>
2023-08-24 04:49:15 +0100

    version one

---
+++ components/shared/LeftSideBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+const LeftSideBar = () => {
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const router = useRouter();
+          const pathname = usePathname();
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && `bg-primary-500`}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                height={24}
+                width={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default LeftSideBar;
